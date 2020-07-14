a42e7cdf0cf4096e8ef4ea329b0c7af744f6a320
ha16t2 <ntha5531@gmail.com>
2020-07-14 16:28:33 +0700

    helloexpress

--- hello/myserve.js
+++
@@ -1,7 +0,0 @@
-var http=require("http");//require http module gan cho bien
-function requestHandler(request,response){
- console.log("In comes a request to:"+request.url);
- response.end("Hello,world!");
-}
-var server=http.createServer(requestHandler);
-server.listen(3000);
--- hello/url-test.js
+++
@@ -1,5 +0,0 @@
-var url=require("url");
-var parseURL=url.parse("http://example.com/profile?name=barry");
-console.log(parseURL.protocol);
-console.log(parseURL.host);
-console.log(parseURL.query);
