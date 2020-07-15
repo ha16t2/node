9a9d8c3506ff8badf0880ad13c5b10fc705cfe24
ha16t2 <ntha5531@gmail.com>
2020-07-15 15:01:51 +0700

    t

--- helloexpress/view.js
+++
@@ -1,8 +0,0 @@
-var express=require("express");
-var path=require("path");
-var app=express();
-//Tells Express that your views will be in a folder called views
-app.set("views",path.resolve(__dirname,"views"));
-//tell express use EJS template
-app.set("view engine","ejs");
-//after chapter
